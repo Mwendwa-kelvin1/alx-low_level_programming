bdfacfa970dc3fffceed2a754c8ae1344f03fdb1
Mwendwa-kelvin1 <kmwendwa692@gmail.com>
2023-07-17 10:27:42 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -9,3 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
